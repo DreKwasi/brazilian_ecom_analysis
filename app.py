404653b0df294a64cc24745b663e2f5d3e885cc2
DreKwasi <andrewsboateng137@gmail.com>
2023-03-15 17:14:38 +0000

    included styling and page config

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from st_pages import Page, Section, show_pages, add_page_title
+from helper_funcs import styles
 
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
+styles.load_css_file("assets/styles/main.css")
+styles.set_png_as_page_bg("assets/img/olist_logo.png")
 
 show_pages([
     Page("app.py", "Home", ),
@@ -9,3 +13,5 @@ show_pages([
     Page("other_pages/forecast.py", "Forecasting", ), 
      
 ])
+
+st.header("Dashboard")
