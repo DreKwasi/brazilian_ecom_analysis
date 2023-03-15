c9f1ac207b0ff32f3f81162dd567329260ff8a22
DreKwasi <andrewsboateng137@gmail.com>
2023-03-15 22:52:37 +0000

    Set Page Config

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from st_pages import Page, Section, show_pages, add_page_title
 from helper_funcs import styles
+from page_funcs import home_view
+
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
@@ -14,4 +16,5 @@ show_pages([
      
 ])
 
-st.header("Dashboard")
+
+home_view.show()
