3438a1bb90c3a4a3669850247ee644368ce4bec3
DreKwasi <andrewsboateng137@gmail.com>
2023-03-15 22:53:25 +0000

    Visualization fo trends

--- page_funcs/home_view.py
+++ page_funcs/home_view.py
@@ -3,80 +3,65 @@ import pandas as pd
 import numpy as np
 import plotly_express as px
 from plotly import graph_objects as go
-from helper_funcs import data_parser
+from helper_funcs import data_parser, st_filters
 from streamlit_extras.metric_cards import style_metric_cards
+from st_vizzu import *
 
 
 def show():
     df = data_parser.read_data()
+    print(df.info())
 
     # Sidebar Filters
     st.sidebar.header("Filters")
+    st.sidebar.write("**View Revenue/Volume**")
+    view = st.sidebar.selectbox(
+        "view", options=["Revenue", "Volume"], key="view", label_visibility="collapsed")
 
-    payment_types = df['payment_type'].unique()
-    st.sidebar.write("**Payment Type**")
-    sel_payment_type = st.sidebar.multiselect(
-        label="payment_type", key="payment_type", options=payment_types, label_visibility="collapsed")
-    if len(sel_payment_type) > 0:
-        df = df[df['payment_type'].isin(sel_payment_type)]
+    filters = st_filters.filter_widgets(df)
+    df = st_filters.filter_data(df, *filters)
 
-    prod_category = df['product_category_name'].unique()
-    st.sidebar.write("**Product Categories**")
-    sel_prod_category = st.sidebar.multiselect(
-        label="product_category_name", key="product_category_name", options=prod_category, label_visibility="collapsed")
-    if len(sel_prod_category) > 0:
-        df = df[df['product_category_name'].isin(sel_prod_category)]
+    total_request_value = df['price'].sum()
+    total_supplied_value = df[df['order_status'].isin(
+        ["delivered", "shipped"])]['price'].sum()
+    fr = np.round(total_supplied_value/total_request_value * 100, 2)
 
-    col_seller_city, col_seller_state = st.sidebar.columns(2)
-
-    with col_seller_city:
-        seller_city = df['seller_city'].unique()
-        st.write("**Seller City**")
-        sel_seller_city = st.multiselect(
-            "seller_city", key="seller_city", options=seller_city, label_visibility="collapsed")
-        if len(sel_seller_city) > 0:
-            df = df[df['seller_city'].isin(sel_seller_city)]
-
-    with col_seller_state:
-        seller_state = df['seller_state'].unique()
-        st.write("**Seller State**")
-        sel_seller_state = st.multiselect(
-            "seller_state", key="seller_state", options=seller_state, label_visibility="collapsed")
-        if len(sel_seller_state) > 0:
-            df = df[df['seller_state'].isin(sel_seller_state)]
-
-    col_cus_city, col_cus_state = st.sidebar.columns(2)
-
-    with col_cus_city:
-        customer_city = df['customer_city'].unique()
-        st.write("**Customer City**")
-        sel_customer_city = st.multiselect(
-            "customer_city", key="customer_city", options=customer_city, label_visibility="collapsed")
-        if len(sel_customer_city) > 0:
-            df = df[df['customer_city'].isin(sel_customer_city)]
-
-    with col_cus_state:
-        customer_state = df['customer_state'].unique()
-        st.write("**Customer State**")
-        sel_customer_state = st.multiselect(
-            "customer_state", key="customer_state", options=customer_state, label_visibility="collapsed")
-        if len(sel_customer_state) > 0:
-            df = df[df['customer_city'].isin(sel_customer_state)]
-
-    # payment_types = df['payment_type'].unique()
-    # st.sidebar.write("**Payment Type**")
-    # payment_type = st.sidebar.multiselect(
-    #     "payment_type", key="payment_type", options=payment_types, label_visibility="collapsed")
-    # if len(payment_type) > 0:
-    #     df = df[df['payment_type'].isin(payment_type)]
-
-    total_request_value = data_parser.clean_format(df['price'].sum())
-
-    st.header("Ecommerce Dashboard")
-    st.subheader("Quick Overview")
+    st.header("Welcome to Your Ecommerce Insights Dashboard")
+    st.subheader("Order Summary")
 
     col1, col2, col3 = st.columns(3, gap="small")
 
-    col1.metric("**Total Requested Value**", value=f"$ {total_request_value}")
+    col1.metric("**Requested Value**",
+                value=f"$ {data_parser.clean_format(total_request_value)}")
+    col2.metric("**Supplied Value**",
+                value=f"$ {data_parser.clean_format(total_supplied_value)}")
+    col3.metric("**Fulfillment Rate**", value=f"{fr} %")
+
+    col1, col2, col3 = st.columns(3, gap="small")
 
     style_metric_cards()
+
+    # tab1, tab2 = st.tabs(["Value", "Volume"])
+    freq = st.selectbox("Select Frequency for Trend", options=["Daily", "Weekly", "Monthly"])
+
+    if view == "Revenue":
+        trend_data = df.groupby(by=[pd.Grouper(
+            key="order_purchase_timestamp", freq=freq[0]), "order_status"]).agg({"price": "sum"}).reset_index()
+        fig = px.line(trend_data, x="order_purchase_timestamp",
+                      y="price", color="order_status", labels={"order_status": "Order Status"},
+                      color_discrete_sequence=px.colors.qualitative.Plotly)
+        fig.update_layout(title=f" {freq} Revenue Trend",
+                          xaxis_title="Order Date", yaxis_title="Revenue")
+        st.plotly_chart(fig, use_container_width=True,
+                        config={"displayModeBar": False})
+
+    if view == "Volume":
+        trend_data = df.groupby(by=[pd.Grouper(
+            key="order_purchase_timestamp", freq=freq[0]), "order_status"]).agg(Number_of_Orders=("order_purchase_timestamp", "count")).reset_index()
+        fig = px.line(trend_data, x="order_purchase_timestamp",
+                      y="Number_of_Orders", color="order_status", labels={"order_status": "Order Status"},
+                      color_discrete_sequence=px.colors.qualitative.Plotly)
+        fig.update_layout(title=f" {freq} Count Trend",
+                          xaxis_title="Order Date", yaxis_title="Number of Orders")
+        st.plotly_chart(fig, use_container_width=True,
+                        config={"displayModeBar": False})
